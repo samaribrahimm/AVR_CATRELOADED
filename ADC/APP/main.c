631ef385f06d0c50f6b6ea53aab94af73b1a6cc3
samar ibrahim <samaribrahim@std.mans.edu.eg>
2024-02-26 22:18:13 +0200

    some modify in ADC

--- ADC/APP/main.c
+++ ADC/APP/main.c
@@ -28,42 +28,47 @@
 
 extern EXTI_t EXTI_AstrEXTIConfig[3];
 
- void Read_POT(void *ptr);
+ volatile void Read_POT(void *ptr);
 
 int main(void)
 {
 	u8 ADC_Read = 0;
-	DIO_enuSetPortDirection(DIO_u8PORTC, 0xFF);
-	DIO_enuSetPinDirection(DIO_u8PORTA, DIO_u8PIN2, DIO_u8INPUT);
+	DIO_enuSetPinDirection(DIO_u8PORTA, DIO_u8PIN2, DIO_u8INPUT); //input ADC
 
-	DIO_enuSetPinDirection(DIO_u8PORTD, DIO_u8PIN2, DIO_u8INPUT);
+	DIO_enuSetPinDirection(DIO_u8PORTD, DIO_u8PIN2, DIO_u8INPUT); //switch
 	DIO_enuSetPinVal(DIO_u8PORTD, DIO_u8PIN2, DIO_u8PULLUP);
 
+	EXTI_enuInit(EXTI_AstrEXTIConfig);
+	EXTI_enuEnableINT(INT0);
+	EXTI_enuSetSenseLevel(INT0, FALLING_EDGE);
 
 
 	ADC_enuInit();
 	ADC_enuSelectChannel(ADC2);
-	ADC_enuDisableTriggerMode();
-
-
+	ADC_enuEnableTriggerMode(EXTI0_TRIG);
+	ADC_enuEnableInterruptrMode();
+	ADC_enuCallBack(Read_POT,&ADC_Read);
 	ADC_enuEnable();
 	ADC_enuStartConversion();
+	LCD_enuInit();
 
+	//DIO_enuSetPortDirection(DIO_u8PORTC, 0xFF);
 
 	GIE_enuGloable_Interrupt_Enable();
 
 
     while (1)
     {
-ADC_enuStartConversion();
+/*ADC_enuStartConversion();
 ADC_enuPollingSystem();
 ADC_enuReadHighValue(&ADC_Read);
-DIO_enuSetPortVal(DIO_u8PORTC, ADC_Read);
-
+DIO_enuSetPortVal(DIO_u8PORTC, ADC_Read);*/
+    	//DIO_enuSetPortVal(DIO_u8PORTC, ADC_Read);
     }
 }
- void Read_POT(void *ptr)
+ volatile void Read_POT(void *ptr)
 {
 	ADC_enuReadHighValue(ptr);
+	LCD_enuDisplayNum(*(s32*)ptr);
 
 }
